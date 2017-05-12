7af3d5e775e5dc0fb202fa44760cdd4eaa39eafa
«First <noteto@rambler.ru>
2017-05-12 15:19:13 +0300

    nazat to local db

--- app/models/home.js
+++ app/models/home.js
@@ -5,8 +5,8 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-//var connectionString =  'postgres://postgres:1@localhost:5432/newdb';
-var connectionString =  DATABASE_URL;
+var connectionString =  'postgres://postgres:1@localhost:5432/newdb';
+//var connectionString =  DATABASE_URL;
 var db = pgp(connectionString);
 
 function getAllUsers(page, cb) {
