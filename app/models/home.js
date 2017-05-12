0826e966ffa16d25136f26d1f68bc1be528cad34
«First <noteto@rambler.ru>
2017-05-12 16:35:00 +0300

    add ssl true

--- app/models/home.js
+++ app/models/home.js
@@ -12,7 +12,8 @@ var connectionString = {
     port: 5432,
     database: 'd75g3o5dlffb85',
     user: 'dzfyusivadobec',
-    password: 'de16e5dc212d220edeb90620e9807b57b392cf17173d51b96eb8b71a1d5fee60'
+    password: 'de16e5dc212d220edeb90620e9807b57b392cf17173d51b96eb8b71a1d5fee60',
+    ssl: true
 };
 var db = pgp(connectionString);
 
